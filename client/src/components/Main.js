0a9e77b23d84d1b1f513ca5344f75811407e3e25
Sofiya-12 <sofiyaki5689@gmail.com>
2024-08-20 22:48:01 +0530

    Changes to be committed:
    	modified:   client/src/components/Main.js
    	new file:   server/models/questionSchema.js
    	new file:   server/models/resultSchema.js

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -20,15 +20,7 @@ export default function Main() {
     <div className='container'>
         <h1 className='title text-light'>Quiz Application</h1>
 
-        <ol>
-            <li>You will be asked 10 questions one after another.</li>
-            <li>10 points is awarded for the correct answer.</li>
-            <li>Each question has three options. You can choose only one options.</li>
-            <li>You can review and change answers before the quiz finish.</li>
-            <li>The result will be declared at the end of the quiz.</li>
-        </ol>
-
-        <form id="form">
+                <form id="form">
             <input ref={inputRef} className="userid" type="text" placeholder='Username*' />
         </form>
 
